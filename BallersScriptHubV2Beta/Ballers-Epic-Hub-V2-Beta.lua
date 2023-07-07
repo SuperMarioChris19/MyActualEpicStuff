ca282d299557653412883eba05e6facdaebacf27
SMChris <134161760+SuperMarioChris19@users.noreply.github.com>
2023-07-06 21:15:59 -0400

    Reverted back bc it made the script not work

--- BallersScriptHubV2Beta/Ballers-Epic-Hub-V2-Beta.lua
+++ BallersScriptHubV2Beta/Ballers-Epic-Hub-V2-Beta.lua
@@ -506,13 +506,12 @@ local function BTCX_fake_script() -- KickAllButton.LocalScript
 	local function kickPlayers()
 		-- Get all players in the game
 		local players = game.Players:GetPlayers()
-	         local localPlayer = game.Players.LocalPlayer
+	
 		-- Loop through each player
 		for _, player in ipairs(players) do
-			if player ~= localPlayer then
+			
 			player:Kick("You have been kicked from the game. Reason: Banned For 1 Day Nah just kidding you were just kicked")
 		end
-	end
 	end
 	
 	-- Connect the function to the GUI button's Click event
